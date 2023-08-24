514bad4452428898f65d56a75febc9db07682a9a
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-23 22:04:26 -0400

    Counter Logic added.

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import ImageDetail from '../components/ImageDetail';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
 
 interface CounterScreenProps {
   navigation: NavigationScreenProp<NavigationState>;
 }
 
 const CounterScreen: React.FC<CounterScreenProps> = () => {
+  const [counter, setCounter] = useState(0)
   return (
     <View>
       <Text>Counter</Text>
+      <Button
+        title="Increase"
+        onPress={_ => setCounter(() => counter + 1)}
+      />
+      <Button
+        title="Decrease"
+        onPress={_ => setCounter(() => counter - 1)}
+      />
+      <Text style={styles.text}>{counter}</Text>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  text: { fontSize: 35 }
+  text: {
+    fontSize: 35,
+    textAlign: 'center',
+    marginTop: 10
+  }
 });
 
 export default CounterScreen;
