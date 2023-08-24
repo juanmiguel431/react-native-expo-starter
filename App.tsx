8d100b312cb74d0714dd230b41b6091d95c88d7e
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-23 20:19:39 -0400

    HomeScreen component added.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -18,3 +21,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const navigator = createStackNavigator({
+  Home: HomeScreen
+}, {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    title: 'App'
+  }
+});
+
+export default createAppContainer(navigator);
