daca958d5f5558aa338c29e50a335b7de3e0033e
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-23 22:58:00 -0400

    Color Screen - Logic added.

--- src/screens/ColorScreen.tsx
+++ src/screens/ColorScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
 import { View, StyleSheet, Button } from 'react-native';
 
@@ -7,12 +7,24 @@ interface ColorScreenProps {
 }
 
 const ColorScreen: React.FC<ColorScreenProps> = () => {
+  const [color, setColor] = useState('rgb(0,0,0)');
   return (
     <View>
-      <Button title="Add a color"/>
+      <Button title="Add a color" onPress={() => setColor(randomRgb)}/>
+      <View
+        style={{ height: 100, width: 100, backgroundColor: color}}
+      />
     </View>
   )
 };
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red},${green},${blue})`;
+}
 
 const styles = StyleSheet.create({});
 
