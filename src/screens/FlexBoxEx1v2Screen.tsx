31545febeb4ce8426430d17235c2ff336853dcfa
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-31 19:21:46 -0400

    FlexBox exercise - Solution two added.

--- src/screens/FlexBoxEx1v2Screen.tsx
+++ src/screens/FlexBoxEx1v2Screen.tsx
@@ -7,16 +7,36 @@ interface FlexBoxEx1V2ScreenProps {
 }
 
 const FlexBoxEx1V2Screen: React.FC<FlexBoxEx1V2ScreenProps> = () => {
-  const [name, setName] = useState('');
   return (
     <ScrollView>
-      <View>
-        <Text>Juan Miguel</Text>
+      <View style={styles.container}>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#e1af0a',
+        }}/>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#ce1d1d',
+          alignSelf: 'center'
+        }}/>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#33c04a',
+          position: 'absolute',
+          top: 0,
+          right: 0
+        }}/>
       </View>
     </ScrollView>
-  )
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewSize: {
+    height: 100,
+    width: 100
+  },
+  container: {}
+});
 
 export default FlexBoxEx1V2Screen;
