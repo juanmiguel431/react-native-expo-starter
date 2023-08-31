3ef21e40ab0666857318a694b75aea3faef5cf6c
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-31 19:17:55 -0400

    FlexBox exercise - Solution one added.

--- src/screens/FlexBoxEx1v1Screen.tsx
+++ src/screens/FlexBoxEx1v1Screen.tsx
@@ -1,22 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 interface FlexBoxEx1V1ScreenProps {
   navigation: NavigationScreenProp<NavigationState>;
 }
 
 const FlexBoxEx1V1Screen: React.FC<FlexBoxEx1V1ScreenProps> = () => {
-  const [name, setName] = useState('');
   return (
     <ScrollView>
-      <View>
-        <Text>Juan Miguel</Text>
+      <View style={styles.container}>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#e1af0a',
+          alignSelf: 'flex-start'
+        }}/>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#ce1d1d',
+          top: 100
+        }}/>
+        <View style={{
+          ...styles.viewSize,
+          backgroundColor: '#33c04a',
+        }}/>
       </View>
     </ScrollView>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewSize: {
+    height: 100,
+    width: 100
+  },
+  container: {
+    height: 400,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
 
 export default FlexBoxEx1V1Screen;
