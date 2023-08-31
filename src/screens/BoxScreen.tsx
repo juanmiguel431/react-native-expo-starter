7612c7b57ba2af6d08b8df87674dd0326cb5fba6
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-30 22:31:18 -0400

    Flex styling - alignItems - center, flex-start, flex-end, flex-stretch

--- src/screens/BoxScreen.tsx
+++ src/screens/BoxScreen.tsx
@@ -9,14 +9,30 @@ interface BoxScreenProps {
 const BoxScreen: React.FC<BoxScreenProps> = () => {
   const [name, setName] = useState('');
   return (
-    <View style={styles.viewStyle}>
-      <View style={styles.textStyle}> {/*Issue if I try to apply this style to a Text Element in Android.*/}
-        <Text>Box Screen</Text>
+    <>
+      <View style={styles.viewStyle}>
+        <View style={styles.textStyle}>
+          <Text>Box Screen</Text>
+        </View>
       </View>
-    </View>
+
+      {/*Flex start*/}
+      <View style={flexStyles.viewStyle}>
+        <View style={flexStyles.textStyle}>
+          <Text>Child 1</Text>
+        </View>
+        <View style={flexStyles.textStyle}>
+          <Text>Child 2</Text>
+        </View>
+        <View style={flexStyles.textStyle}>
+          <Text>Child 3</Text>
+        </View>
+      </View>
+    </>
   )
 };
 
+// Issue if I try to apply this style to a Text Element in Android.
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
@@ -26,6 +42,20 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     borderColor: 'red',
     margin: 20,
+  }
+});
+
+
+// Flex start
+const flexStyles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    alignItems: 'center'
+  },
+  textStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
   }
 });
 
